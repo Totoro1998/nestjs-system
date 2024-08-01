c0724e16070d0f2a903026ca96887177df13f851
totoro <totoro.fu@webeye.com>
2024-08-01 16:59:54 +0800

    装饰器一览

--- 2.decorator/1.ts
+++ 2.decorator/1.ts
@@ -1,11 +1,12 @@
-interface Obj{
-    a:number;
-    b:number
+// Reflect
+interface Obj {
+  a: number;
+  b: number;
 }
-const obj:Obj = {a:1,b:2}
-console.log(Reflect.get(obj,'a'))
-Reflect.set(obj,'b',2)
-console.log(obj.b)
+const obj: Obj = { a: 1, b: 2 };
+console.log(Reflect.get(obj, "a"));
+Reflect.set(obj, "b", 2);
+console.log(obj.b);
 //deleteProperty
 //has
 //defineProperty
