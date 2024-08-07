fa2daa7fd1e13dc87517fab3695a1100faa2ac09
totoro <totoro.fu@webeye.com>
2024-08-07 14:16:11 +0800

    12.filter

---
+++ 12.filter/doc/11.js
@@ -0,0 +1,9 @@
+let set = new Set();
+set.add("A");
+set.add("B");
+set.add("A");
+set.add("A");
+set.add("A");
+set.add("A");
+if (!set.has("A")) set.add("A");
+console.log(set);
---
+++ 12.filter/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { CustomExceptionFilter } from "./custom-exception.filter";
+import { APP_FILTER } from "@nestjs/core";
+@Module({
+  controllers: [AppController],
+  providers: [
+    {
+      provide: "PREFIX",
+      useValue: "prefix",
+    },
+    {
+      provide: APP_FILTER,
+      useClass: CustomExceptionFilter,
+    },
+  ],
+})
+export class AppModule {}
