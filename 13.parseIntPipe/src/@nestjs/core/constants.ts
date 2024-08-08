f87613a88e4fd9be0b1f9d4c8d1b77e5d6ebc080
totoro <totoro.fu@webeye.com>
2024-08-08 19:22:46 +0800

    13.pipe

--- 13.parseIntPipe/src/@nestjs/core/constants.ts
+++
@@ -1,1 +0,0 @@
-export const APP_FILTER = 'APP_FILTER'
--- 13.parseIntPipe/src/app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get, Param, ParseIntPipe } from "@nestjs/common";
-
-@Controller()
-export class AppController {
-  @Get("number/:id")
-  getNumber(@Param("id", ParseIntPipe) id: number) {
-    return `The number is ${id}`;
-  }
-}
