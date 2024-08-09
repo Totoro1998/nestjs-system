076ec239c0accdb0b8cd70aed231d5bdd3add3ac
totoro <totoro.fu@webeye.com>
2024-08-09 10:01:53 +0800

    代码优化

--- 14.guard/src/auth.guard.ts
+++ 14.guard/src/auth.guard.ts
@@ -6,9 +6,9 @@ export class AuthGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    //从处理程序的方法的元数据上获取角色信息
+    // 从处理程序的方法的元数据上获取角色信息
     const roles = this.reflector.get("roles", context.getHandler());
-    //如果没有为此路由处理函数设置角色，那表示此路由可以被 任何人访问
+    // 如果没有为此路由处理函数设置角色，那表示此路由可以被任何人访问
     if (!roles) {
       return true;
     }
--- 14.guard/src/auth.middleware.ts
+++ 14.guard/src/auth.middleware.ts
@@ -7,12 +7,11 @@ export class AuthMiddleware implements NestMiddleware {
     /**
      * 一般会在此处给req.user赋值
      * 如何赋值关键要看你鉴权使用哪种方法，一般有两种 session JWT
-     *
      */
-    //如果使用上次用户登录后把用户信息保存在session中的话
-    //req.user = req.session.user;
-    //如果是用的JWT的话
-    //const token -> user -> req.user =
+    // 如果使用上次用户登录后把用户信息保存在session中的话
+    // req.user = req.session.user;
+    // 如果是用的JWT的话
+    // const token -> user -> req.user =
     req.user = { id: 1, name: "nick", roles: [req.query.role] };
     next();
   }
