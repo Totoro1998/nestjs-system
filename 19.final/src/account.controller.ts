6a9ea26e637eede7cc5a2f8188501846c88708d9
totoro <totoro.fu@webeye.com>
2024-08-09 17:15:31 +0800

    19.final

---
+++ 19.final/src/account.controller.ts
@@ -0,0 +1,17 @@
+import { Get, Controller, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "./auth.guard";
+import { Auth2Guard } from "./auth2.guard";
+import { Roles } from "./roles.decorator";
+import { Roles2 } from "./roles2.decorator";
+@Controller("accounts")
+//@UseGuards(AuthGuard)
+export class AccountController {
+  @Get()
+  //@UseGuards(AuthGuard)
+  //@UseGuards(Auth2Guard)
+  @Roles("admin", "super") //roles ['admin','super']
+  //@Roles2(['admin','super'])// Roles2 ['admin','super']
+  async index() {
+    return `this aciton return all accounts`;
+  }
+}
