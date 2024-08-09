f1d9dc9319ed6a9d180c824f4065823a6cf55f1e
totoro <totoro.fu@webeye.com>
2024-08-09 09:50:08 +0800

    account.controller

--- 14.guard/src/account.controller.ts
+++ 14.guard/src/account.controller.ts
@@ -3,8 +3,8 @@ import { AuthGuard } from "./auth.guard";
 import { Roles } from "./roles.decorator";
 @Controller("accounts")
 export class AccountController {
-  @Get()
   @UseGuards(AuthGuard)
+  @Get()
   @Roles("admin", "superadmin") //此装饰器的作用是给当前的index函数添加了角色数组的元数据
   async index() {
     return `this aciton return all accounts`;
