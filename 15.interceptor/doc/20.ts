48e2a1b7e7359d5ad24a07b9ec064a773b6af779
totoro <totoro.fu@webeye.com>
2024-08-09 11:10:45 +0800

    代码优化

--- 15.interceptor/doc/20.ts
+++ 15.interceptor/doc/20.ts
@@ -1,21 +1,21 @@
 //扩展基本的函数行为
 
-function extendBehavior(target,propertyName,descriptor){
-    const originalMethod = descriptor.value;
-    descriptor.value = function(...args){
-      console.log(`extends....`)
-      const result  = originalMethod.apply(this,args);
-      return result;
-    }
+function extendBehavior(target, propertyName, descriptor) {
+  const originalMethod = descriptor.value;
+  descriptor.value = function (...args) {
+    console.log(`extends....`);
+    const result = originalMethod.apply(this, args);
+    return result;
+  };
+}
+
+class Example {
+  @extendBehavior
+  baseMethod() {
+    console.log("Executing baseMethod");
   }
-  
-  class Example{
-      @extendBehavior
-      baseMethod(){
-          console.log('Executing baseMethod')
-      }
-  }
-  const example = new Example();
-  example.baseMethod();
-  
-  export {}
+}
+const example = new Example();
+example.baseMethod();
+
+export {};
